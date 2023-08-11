fb0879dcfc79458e150f48f9c49791807449d3df
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-08-11 22:44:43 +0100

    new file:   0x02-python-import_modules/2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    count = len(argv)
+    i = 1
+
+    if count == 1:
+        print("{} arguments.".format(count - 1))
+    elif count < 2:
+        print("{} argument:".format(count-1))
+    else:
+        print("{} arguments:".format(count-1))
+
+    while (i < count):
+        print("{}: {}".format(i, argv[i]))
+        i += 1
