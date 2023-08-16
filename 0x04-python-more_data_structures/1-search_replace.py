1237fccd1e744d4f92ea34b71b57d14a0a99967f
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-08-16 22:31:44 +0100

    Task 1 completed

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        new = my_list.copy()
+
+        for n in new:
+            if n == search:
+                i = new.index(n)
+                new[i] = replace
+
+    return new
