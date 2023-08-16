8c0958955b97a06951f4eb4d44462d9cb3ececf0
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-08-16 13:46:27 +0100

    Task 2 completed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 def element_at(my_list, idx):
     count = len(my_list)
 
-    if(idx < 0):
+    if idx < 0:
         return None
-    elif(idx >= count):
+    elif idx >= count:
         return None
     else:
         return my_list[idx]
