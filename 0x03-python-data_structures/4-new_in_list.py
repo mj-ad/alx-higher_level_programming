d2945c703ab4fd8a59e4607695764036f54497bf
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-08-16 14:01:28 +0100

    Task 4 completed

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        count = len(my_list)
+        new_list = my_list.copy()
+
+        if idx < 0:
+            return my_list
+        elif idx >= count:
+            return my_list
+        else:
+            new_list[idx] = element
+            return new_list
